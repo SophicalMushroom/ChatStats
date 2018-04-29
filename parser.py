062138aeef8c17abad8a6b10148be1294dc86dc4
Dittam <31997684+Dittam@users.noreply.github.com>
2018-04-29 15:57:44 -0400

    added support for new facebook archive format

--- parser.py
+++ parser.py
@@ -15,33 +15,33 @@ def find_between(string, first, last):
 
 def str2numdate(datestring):
     '''convert string representation of a date to YYYY-MM-DD
-    'Wednesday, February 14, 2018 at 5:45pm EST' --> '2018-02-14'
+    'Apr 28, 2018 4:20pm' --> '2018-02-14'
     '''
     num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    cal = {'': 0, 'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
-           'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+    cal = {'': 0, 'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'June': 6,
+           'July': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
 
     datestring = datestring.split(' ')
-    datestring[1] = cal[datestring[1]]
+    datestring[0] = cal[datestring[0]]
     after = []
 
-    after.append(str(datestring[3]) + '-')
+    after.append(str(datestring[2]) + '-')
 
-    if str(datestring[1]) in num:
-        after.append('0' + str(datestring[1]) + '-')
+    if str(datestring[0]) in num:
+        after.append('0' + str(datestring[0]) + '-')
     else:
-        after.append(str(datestring[1]) + '-')
+        after.append(str(datestring[0]) + '-')
 
-    if str(datestring[2]).strip(',') in num:
-        after.append('0' + str(datestring[2]).strip(','))
+    if str(datestring[1]).strip(',') in num:
+        after.append('0' + str(datestring[1]).strip(','))
     else:
-        after.append(str(datestring[2]).strip(','))
+        after.append(str(datestring[1]).strip(','))
 
     return''.join(after)
 
 
 #-----get raw message data from file-----
-with open('RawData.txt', 'r', encoding='utf-8') as f:
+with open('C:/Users/ditta/Desktop/messages/UTSCCMSClassof2021_b599914f0f/message.html', 'r', encoding='utf-8') as f:
     lines = f.read()
 
 #-----Remove non-utf-8 chars-----
@@ -49,7 +49,8 @@ normalized = unicodedata.normalize('NFKD', lines).encode('ascii', 'ignore')
 decoded_text = normalized.decode('utf-8')
 
 #-----split into list of messages with their metadata-----
-decoded_text = decoded_text.split('<div class="message">')
+decoded_text = decoded_text.split(
+    '</div><div class="pam _3-95 _2pi0 _2lej uiBoxWhite noborder">')
 del decoded_text[0]
 
 metadata = {
@@ -68,22 +69,17 @@ for message in decoded_text:
 
     # get user that sent the message
     metadata['users'].append(find_between(
-        message, '<span class="user">', '</span>'))
+        message, '<div class="_3-96 _2pio _2lek _2lel">', '</div>'))
 
     # convert date to something readable
-    rawDate = find_between(message, '<span class="meta">', '</span>')
+    rawDate = find_between(message, '<div class="_3-94 _2lem">', '</div>')
     #datenum = dates.datestr2num(str2numdate(rawDate))
     dateReadable = str2numdate(rawDate)
     metadata['date'].append(dateReadable)
 
-    messageText = (find_between(message, '</div></div><p>', '</p>'))
-
-    for tags in filters:
-        if tags in messageText:
-            messageText = ' '
-    # filter for when someone @user message
-    if '<ul class' in messageText:
-        messageText = find_between(messageText, '<p>', '<ul')
+    # extract message
+    messageText = (find_between(
+        message, '<div class="_3-96 _2let"><div><div>', '</div>'))
 
     metadata['messages'].append(messageText)
 
